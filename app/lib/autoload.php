2970eaf65192f32e793c1cd895ae4f48bf66244a
E3N-Geek <95266012+E3N-Geek@users.noreply.github.com>
2024-04-05 10:24:41 +0700

    Delete app/lib directory

--- app/lib/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/altorouter/autoload.php';
-require_once __DIR__ . '/mpdf/autoload.php';
-require_once __DIR__ . '/mpdf/mpdf/qrcode/vendor/autoload.php';
-require_once __DIR__ . '/PHPMailer/src/Exception.php';
-require_once __DIR__ . '/PHPMailer/src/PHPMailer.php';
-require_once __DIR__ . '/PHPMailer/src/SMTP.php';
